829d1c3e14f2b3a419125771e95e03c47c88b9cd
Frupla <s163920@student.dtu.dk>
2017-06-09 10:31:40 +0200

    exercise 8

--- main.c
+++ main.c
@@ -1,1 +1,16 @@
+#include <eZ8.h>  						  // special encore constants, macros and flash functions
+#include <sio.h>	                      // special encore serial i/o functions
+#include "ansi.h"
+#include "LEDdisplay.h"
 
+rom char string[LED_MAX_STR_LEN] = "    sample text";
+
+void main() {
+	init_uart(_UART0,_DEFFREQ,_DEFBAUD);  // set-up UART0 to 57600, 8n1
+	LEDinit();
+	LEDsetstring(string);
+	LEDscroll();
+	do {
+		LEDupdate();
+	} while (1 != 2);                  // stay here forever
+}
