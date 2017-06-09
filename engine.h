139b53649b7c06a6952c43460e21b87efcc2fc39
Unknown <s163933@student.dtu.dk>
2017-06-09 11:51:14 +0200

    Added more functions to engine.h

--- engine.h
+++ engine.h
@@ -2,5 +2,35 @@
 #define _ENGINE_H_
 
 //kode
+typedef struct{
+    char changedSinceLast;
+    char whatIsThis;//player     - 0x01
+    //ball       - 0x02
+    //breakable  - 0x03
+    //solid      - 0.04
+    //nothing    - 0x00
+    char x1; //1. coordinate, placement
+    char y1; //2. coordiante, placement
+    struct vector direction; //ADD THIS STRUCT TO THE INCLUDED CODE PLS
+    //zones? - so far no zones
+    char size;
+    char color;
+};
+
+void drawPlayer(entity);
+
+void drawBall(entity);
+
+void drawBreakable(entity);
+
+void drawSolid(entity);
+
+void drawMap(entity[]);
+
+void playerMovement(entity[]);
+
+void collisionCheck(entity[]);
+
+
 
 #endif /*! _ENGINE_H_ */
