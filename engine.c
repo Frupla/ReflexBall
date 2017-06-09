17ea11b59ea64cbf27f874777d6e8f25d0034737
Frupla <s163920@student.dtu.dk>
2017-06-09 11:14:51 +0200

    inclusions

---
+++ engine.c
@@ -0,0 +1,6 @@
+#include <eZ8.h>  						  // special encore constants, macros and flash functions
+#include <sio.h>	                      // special encore serial i/o functions
+#include "ansi.h"
+#include "LEDdisplay.h"
+
+//kode
