ca754d64a2858f10379f9f65cd89def7872183e7
Notapirateyet <s163933@student.dtu.dk>
2017-06-12 15:09:46 +0200

    Makes the ball go round

--- test.c.c
+++ test.c.c
@@ -79,12 +79,8 @@ void main() {
         } while (readMsec() < time2);
         //Then move the ball
         ballMovement(map);
-        rotate(&map[1].direction, 16);
-        //rotate(&(map[1].direction), 32);
-        //Check for collisions
-        //collisionCheck(map);
-        //Update the map
-        //drawMap(map);
+        rotate(&(map[1].direction), 32);
+
         LEDUpdate();
         time2 += 500;
         if (readMsec() < 30) {
