338cf6cc974818087c3e1d63bb7f2c592fc3c680
Frupla <s163920@student.dtu.dk>
2017-06-14 10:13:30 +0200

    nu med flere bloks

--- test.c.c
+++ test.c.c
@@ -15,7 +15,7 @@ rom char string[LED_MAX_STR_LEN] = "Shit";
 void main() {
     player_t player[2];
     ball_t ball[2];
-    breakable_t breakable[3];
+    breakable_t breakable[60];
     int i, j;
     int n = 0;
     Tvector tempVec;
@@ -38,8 +38,8 @@ void main() {
     rotate(&tempVec, 47);
     ball[0].whatIsThis = 0x02;
     ball[0].changedSinceLast = 1;
-    ball[0].x1 = LONG_TO_EIGHTEEN_FOURTEEN(7);
-    ball[0].y1 = LONG_TO_EIGHTEEN_FOURTEEN(10);
+    ball[0].x1 = LONG_TO_EIGHTEEN_FOURTEEN(50);
+    ball[0].y1 = LONG_TO_EIGHTEEN_FOURTEEN(60);
     ball[0].direction = tempVec;
     ball[0].size = 0x00;
     ball[0].color = 0x00;
@@ -47,13 +47,13 @@ void main() {
 	i = 5;
 	j = 5;
     //Breakable setup
-    for(i = 10; i <= 90; i += 40){
-    	for(j = 4; j <= 8; j += 4){
+    for(i = 4; i <= 104; i += 20){
+    	for(j = 4; j <= 20; j += 2){
             breakable[n].whatIsThis = 0x03;
-            breakable[n].changedSinceLast = 1;
+            breakable[n].changedSinceLast = 1;  
             breakable[n].x1 = i;
             breakable[n].y1 = j;
-            breakable[n].sizeX = 0x16;
+            breakable[n].sizeX = 0x14;
             breakable[n].sizeY = 0x01;
             breakable[n].lives = 0x03;
             n++;
